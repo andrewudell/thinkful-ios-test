e6c1a7b23adcd8e043e5a8faa5db15e5d6d214d8
Andrew Udell <andrewk.udell@gmail.com>
2014-09-07 22:16:22 -0400

    Existing repo

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,23 @@
+let otherMysteryMan: String? = "A dark figure in a cape."
+
+println(otherMysteryMan!)
+
+func yolo() {
+    println("Hi")
+}
+
+yolo()
+yolo()
+yolo()
+
+class CarFactory {
+    var color = ""
+    var horsepower = 0
+    var automaticOption = false
+}
+
+
+var myCar = CarFactory()
+
+println(myCar.horsepower)
+
